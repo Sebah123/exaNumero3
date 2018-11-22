3b28e0cc10a2d03698004c8a1c0eaed5e9cb9e48
Flavio Toro B <38701988+fulkesko@users.noreply.github.com>
2018-11-22 09:59:29 -0300

    script
    bases iniciales

--- ProyectoAutomotriz/src/org/app/model/script.sql
+++ ProyectoAutomotriz/src/org/app/model/script.sql
@@ -1,10 +1,24 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Author:  Flavio
- * Created: 22/11/2018
- */
+CREATE DATABASE prueba3_poo;
 
+USE prueba3_poo;
+
+CREATE TABLE automovil(
+    patente VARCHAR(7),
+    marca VARCHAR(100),
+    estado BOOLEAN, -- true(nuevo) false(usado)
+    precio LONG,
+    red INT,
+    green INT,
+    blue INT,
+    PRIMARY KEY(patente)
+);
+
+INSERT INTO automovil VALUES('AABB-11','Toyota',true,12500000,255,0,0);
+INSERT INTO automovil VALUES('CCDD-22','Chevrolet',true,8900000,0,255,0);
+INSERT INTO automovil VALUES('EEFF-33','Peugeot',false,650000,0,0,255);
+
+SELECT * FROM automovil ORDER BY precio DESC;
+
+-- Eliminar la base de datos
+-- DROP DATABASE prueba3_poo;
+-- Eliminar la base de datos
